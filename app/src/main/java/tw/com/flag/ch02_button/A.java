33455e4b798641aeaab0f0a5b76705ddfcba39ab
ChengHsien Yang <chenghyang2001@yahoo.com>
2017-09-21 16:53:46 +0800

    code clean at 4:53 pm. ok

--- app/src/main/java/tw/com/flag/ch02_button/A.java
+++ app/src/main/java/tw/com/flag/ch02_button/A.java
@@ -1,23 +1,8 @@
 package tw.com.flag.ch02_button;
 
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.os.Environment;
 import android.util.Log;
-import android.widget.Toast;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-/**
- * @author peter
- */
 public class A {
 
     public static void a() {
