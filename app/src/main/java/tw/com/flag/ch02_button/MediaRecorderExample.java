cb124230bdb8b06b3500530da66d4009870ca560
ChengHsien Yang <chenghyang2001@yahoo.com>
2017-09-28 10:28:56 +0800

    add MyReceiver class to receive BOOT_COMPLETED broadcast message from system.

--- app/src/main/java/tw/com/flag/ch02_button/MediaRecorderExample.java
+++ app/src/main/java/tw/com/flag/ch02_button/MediaRecorderExample.java
@@ -1,6 +1,8 @@
 package tw.com.flag.ch02_button;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
@@ -284,6 +286,15 @@ public class MediaRecorderExample extends Activity {
 
 
 
+    }
+
+
+    public static class MyReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            A.a("Action: " + intent.getAction());
+        }
     }
 
 }
